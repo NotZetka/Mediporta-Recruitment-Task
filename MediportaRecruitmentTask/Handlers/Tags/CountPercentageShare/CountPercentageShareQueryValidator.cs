66a284520431e682ab7ea5b11dbc375e7aeb5d66
Zetka <jakub.kochanski10@gmail.com>
2024-04-03 23:58:22 +0200

    added validator

---
+++ MediportaRecruitmentTask/Handlers/Tags/CountPercentageShare/CountPercentageShareQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Mediporta_Recruitment_Task.Handlers.Tags.CountPercentageShare
+{
+    public class CountPercentageShareQueryValidator : AbstractValidator<CountPercentageShareQuery>
+    {
+        public CountPercentageShareQueryValidator()
+        {
+            RuleFor(x => x.Size).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.Page).GreaterThanOrEqualTo(1);
+        }
+    }
+}
